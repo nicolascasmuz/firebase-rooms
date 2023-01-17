f150ac0a07a274e4a84fb920b521862bbb67f6ef
nicolascasmuz <nicolascasmuz@hotmail.com>
2023-01-17 18:08:48 -0300

    commit-002

--- index.ts
+++ index.ts
@@ -13,6 +13,84 @@ app.use(cors());
 const usersCollection = firestore.collection("users");
 const roomsCollection = firestore.collection("rooms");
 
+/* //post auth
+app.post("/auth", (req, res) => {
+  const { email } = req.body;
+
+  usersCollection
+    .where("email", "==", email)
+    .get()
+    .then((searchResponse) => {
+      if (searchResponse.empty) {
+        res.status(404).json({
+          message: "not found",
+        });
+      } else {
+        res.json({
+          id: searchResponse.docs[0].id,
+        });
+      }
+    });
+}); */
+
+/* //post rooms
+app.post("/rooms", (req, res) => {
+  const { userId } = req.body;
+
+  usersCollection
+    .doc(userId.toString())
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        const roomRef = rtdb.ref("rooms/" + nanoid());
+        roomRef
+          .set({
+            messages: [],
+            owner: userId,
+          })
+          .then(() => {
+            const roomLongId = roomRef.key;
+            const roomId = 1000 + Math.floor(Math.random() * 999);
+            roomsCollection
+              .doc(roomId.toString())
+              .set({ rtdbRoomId: roomLongId })
+              .then(() => {
+                res.json({ id: roomId.toString() });
+              });
+          });
+      } else {
+        res.status(401).json({
+          message: "doesn't exist",
+        });
+      }
+    });
+}); */
+
+/* //get rooms:id
+app.get("/rooms/:roomId", (req, res) => {
+  const { userId } = req.query;
+  const { roomId } = req.params;
+
+  userCollection
+    .doc(userId.toString())
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        roomsCollection
+          .doc(roomId)
+          .get()
+          .then((snap) => {
+            const data = snap.data();
+            res.json(data);
+          });
+      } else {
+        res.status(401).json({
+          message: "doesn't exist",
+        });
+      }
+    });
+}); */
+
 // post rooms
 app.post("/rooms", function (req, res) {
   const longRoomId = req.body.longRoomId;
