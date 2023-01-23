fdf3c5dd1774ba42a419bcaae66ecad6c55ff5cb
nicolascasmuz <nicolascasmuz@hotmail.com>
2023-01-23 13:29:10 -0300

    commit-004

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 import { rtdb, firestore } from "./db";
 import * as express from "express";
-import { v4 as uuidv4 } from "uuid";
 import { nanoid } from "nanoid";
 import * as cors from "cors";
 
@@ -13,47 +12,24 @@ app.use(cors());
 const usersCollection = firestore.collection("users");
 const roomsCollection = firestore.collection("rooms");
 
-//post signup
-app.post("/signup", (req, res) => {
-  const email = req.body.email;
-  const nombre = req.body.nombre;
-
-  usersCollection
-    .where("email", "==", email)
-    .get()
-    .then((searchResponse) => {
-      if (searchResponse.empty) {
-        usersCollection
-          .add({
-            email,
-            nombre,
-          })
-          .then((newUserRef) => {
-            res.json({
-              id: newUserRef.id,
-              new: true,
-            });
-          });
-      } else {
-        res.status(400).json({
-          message: "user already exists",
-        });
-      }
-    });
-});
-
-//post auth
+// post auth
 app.post("/auth", (req, res) => {
   const { email } = req.body;
+  const { fullname } = req.body;
 
   usersCollection
     .where("email", "==", email)
     .get()
     .then((searchResponse) => {
       if (searchResponse.empty) {
-        res.status(404).json({
-          message: "not found",
-        });
+        const fullUserId = nanoid();
+        const userId = fullUserId.slice(16);
+        usersCollection
+          .doc(userId.toString())
+          .set({ email: email, fullname: fullname, userId: fullUserId })
+          .then(() => {
+            res.json({ id: userId.toString() });
+          });
       } else {
         res.json({
           id: searchResponse.docs[0].id,
@@ -62,40 +38,7 @@ app.post("/auth", (req, res) => {
     });
 });
 
-/* //post rooms
-app.post("/rooms", (req, res) => {
-  const { userId } = req.body;
-
-  usersCollection
-    .doc(userId.toString())
-    .get()
-    .then((doc) => {
-      if (doc.exists) {
-        const roomRef = rtdb.ref("rooms/" + nanoid());
-        roomRef
-          .set({
-            messages: [],
-            owner: userId,
-          })
-          .then(() => {
-            const roomLongId = roomRef.key;
-            const roomId = 1000 + Math.floor(Math.random() * 999);
-            roomsCollection
-              .doc(roomId.toString())
-              .set({ rtdbRoomId: roomLongId })
-              .then(() => {
-                res.json({ id: roomId.toString() });
-              });
-          });
-      } else {
-        res.status(401).json({
-          message: "doesn't exist",
-        });
-      }
-    });
-}); */
-
-//post rooms 2.0
+// post rooms
 app.post("/rooms", (req, res) => {
   const { userId } = req.body;
 
@@ -128,7 +71,7 @@ app.post("/rooms", (req, res) => {
     });
 });
 
-//get rooms:id
+// get rooms
 app.get("/rooms/:roomId", (req, res) => {
   const { userId } = req.query;
   const { roomId } = req.params;
@@ -153,61 +96,30 @@ app.get("/rooms/:roomId", (req, res) => {
     });
 });
 
-// post rooms
-app.post("/rooms", function (req, res) {
-  const longRoomId = req.body.longRoomId;
-  const shortRoomId = req.body.shortRoomId;
+// get existing room
+app.get("/room/:roomId", (req, res) => {
+  const { roomId } = req.params;
 
-  roomsCollection.doc(shortRoomId.toString()).set({ fullRoomId: longRoomId });
-});
-
-// post users
-app.post("/users", function (req, res) {
-  const longUserId = req.body.longUserId;
-  const shortUserId = req.body.shortUserId;
-  const userEmail = req.body.userEmail;
-  const userFullname = req.body.userFullname;
-
-  usersCollection
-    .doc(shortUserId.toString())
-    .set({ userId: longUserId, email: userEmail, fullname: userFullname });
+  roomsCollection
+    .doc(roomId.toString())
+    .get()
+    .then((roomData) => {
+      res.json(roomData.data());
+    });
 });
 
 // post messages
 app.post("/messages", (req, res) => {
   const { rtdbRoomId } = req.body;
-  const { messages } = req.body;
+  const { message } = req.body;
 
   const roomMessagesRef = rtdb.ref("rooms/" + rtdbRoomId + "/messages");
-  roomMessagesRef.push(messages);
+  roomMessagesRef.push(message);
 });
-
-// get rooms
-/* app.get("/rooms/:roomId", function (req, res) {
-  const existingRoom = req.params.roomId;
-
-  roomsCollection
-    .get()
-    .then((roomsSnap) => {
-      const roomsDoc = roomsSnap.docs;
-      const foundRoom = roomsDoc.find((r) => {
-        const path = r.ref.path.slice(6);
-        return path == existingRoom;
-      });
-      return foundRoom;
-    })
-    .then((slicedPath) => {
-      res.json(slicedPath);
-    });
-}); */
 
 // get messages
 app.get("/rooms/messages/:roomId", function (req, res) {
   const roomId = req.params.roomId;
-
-  /*  const roomRef = rtdb.ref("rooms/" + roomId + "/messages");
-  roomRef.get().then((snap) => {
-    res.json(snap); */
 
   var roomRef = rtdb.ref("rooms/" + roomId + "/messages");
   roomRef.on("value", (snapshot) => {
